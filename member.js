cf6284f0427301e9a616bdddb18ed73465ba3d49
Oscar JR Cardenas <68070683+OscarCarde@users.noreply.github.com>
2024-03-15 09:48:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  let member = {
+    name: 'John',
+    age: 30,
+    skills: ['JS', 'React', 'Node']
+  };
+
+  console.log(member.skills);
+  console.log(member.skills[0]);
+  console.log(member.skills[1]);
+  console.log(member.skills[2]);
+}
